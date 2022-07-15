b97a6612deea6a626278c88406842b592bfe472f
Alcir Daluz <alcirdaluz@protonmail.com>
2022-07-15 09:03:00 -0300

    click events

--- script.js
+++ script.js
@@ -1,11 +1,3 @@
-var hora = 9;
-
-if (hora >= 0 && hora <= 12) {
-  window.alert("Bom dia");
-} else if (hora >= 13 && hora <= 18) {
-  window.alert("Boa tarde");
-} else if (hora >= 19 && hora <= 24) {
-  window.alert("Boa noite");
-} else {
-  window.alert("Horário incorreto");
+function clicou() {
+  window.alert("Obrigado!");
 }
