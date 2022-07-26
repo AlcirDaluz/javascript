14bc7d86142f52b82e436c8a9ed383f5cdc75395
Alcir Daluz <alcirdaluz@protonmail.com>
2022-07-26 17:23:24 -0300

    navigation bar

--- script.js
+++ script.js
@@ -1,20 +1,9 @@
-function azul() {
-  limpar();
-  document.getElementById("titulo").classList.add('azul');
-}
+function menuToggle() {
+  let menuArea = document.getElementById("menu-area");
 
-function vermelho() {
-  limpar();
-  document.getElementById("titulo").classList.add('vermelho');
-}
-
-function verde() {
-  limpar();
-  document.getElementById("titulo").classList.add('verde');
-}
-
-function limpar() {
-  document.getElementById("titulo").classList.remove("azul");
-  document.getElementById("titulo").classList.remove("vermelho");
-  document.getElementById("titulo").classList.remove("verde");
+  if ((menuArea.style.width == "200px") == true) {
+    menuArea.style.width = "0px";
+  } else {
+    menuArea.style.width = "200px";
+  }
 }
